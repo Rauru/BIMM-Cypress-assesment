57744b9ac1eb8dc3d7ebd9b361a965435d720342
Rauru <raul@lumiant.io>
2026-09-19 10:50:06 -0600

    centralized selectors for demoqanavigation

--- page-controllers/base-controllers/DemoqaNavigation.js
+++ page-controllers/base-controllers/DemoqaNavigation.js
@@ -1,24 +1,29 @@
 const exactText = (text) => new RegExp(`^${Cypress._.escapeRegExp(text)}$`);
 
+const selectors = {
+  categoryCard: '.top-card',
+  menuItem: '.menu-list .text',
+};
+
 class DemoqaPage{
 
   checkCardIsVisible(card) {
-     cy.contains('.top-card', exactText(card)).should('be.visible');
+     cy.contains(selectors.categoryCard, exactText(card)).should('be.visible');
      return this;
   }
 
   clickCategoryCard(card){
-    cy.contains('.top-card', exactText(card)).click();
+    cy.contains(selectors.categoryCard, exactText(card)).click();
     return this;
   }
 
   checkMenuItemIsVisible(name){
-    cy.contains('.menu-list .text', exactText(name)).should('be.visible');
+    cy.contains(selectors.menuItem, exactText(name)).should('be.visible');
     return this;
   }
 
   clickMenuItem(name){
-    cy.contains('.menu-list .text', exactText(name)).click();
+    cy.contains(selectors.menuItem, exactText(name)).click();
     return this;
   }
 }
